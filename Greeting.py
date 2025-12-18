9aa544e021ed370c73c006b1419f5c508c7436c8
Madhumitha212 <166145216+Madhumitha212@users.noreply.github.com>
2025-12-18 15:45:01 +0530

    Update Greeting.py

--- Greeting.py
+++ Greeting.py
@@ -2,4 +2,4 @@ user_name = input("Enter name:")
 if len(user_name)>=3:
     print(f"Hello",user_name,", How are you ?");
 else:
-    print("User name should contain at least 2 characters");
+    print("User name should contain at least 3 characters");

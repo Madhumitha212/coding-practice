606ef4eab56e65b3eb58f10e67235630ff752789
Madhumitha212 <madhuravi324@gmail.com>
2026-01-07 16:11:12 +0530

    setup:[Madhu:] - modified check_leap_year

---
+++ check_leap_year.py
@@ -0,0 +1,10 @@
+year = input("Enter year: ")
+
+if len(year) != 4:
+    print("Invalid Digit")
+else:
+    year = int(year)
+    if 
+        print(f"{year} is a Leap Year")
+    else:
+        print(f"{year} is not a Leap Year")
